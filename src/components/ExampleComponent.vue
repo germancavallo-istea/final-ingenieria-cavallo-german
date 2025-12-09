f8df762895e846dea6e4a8a47b5b8b65a9861721
germanvc <german.cavalloo@gmail.com>
2025-12-09 19:48:30 -0300

    Inicio proyecto

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>Pagina de landing de Final de German Cavallo</p>
+    <p>Link utiles en menu arriba a la izquierda</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-layout view="LHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title> Final Ingenieria Software (German Cavallo) </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+
+const linksList: EssentialLinkProps[] = [
+  {
+    title: 'Pagina Istea',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://www.istea.edu.ar',
+  },
+  {
+    title: 'Github con final',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework',
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
